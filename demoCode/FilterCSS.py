47d72ddff66de5f6c3c257fc58eae848852f1353
wangjianfeng <alexwang1982@126.com>
2018-02-13 14:02:56 +0800

    bs4 demo

---
+++ demoCode/FilterCSS.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Q1mi"
+# Date: 2018/2/12
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.error import HTTPError
+
+def getInfoByFilterCSS(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsobj = BeautifulSoup(html)
+        nameList = bsobj.findAll("span",{"class":"green"})
+        for name in nameList:
+            print(name.get_text())
+    except AttributeError as e:
+        return None
+
+    return title
+
+getInfoByFilterCSS("http://www.pythonscraping.com/pages/warandpeace.html")
+
--- demoCode/HtmlRead.py
+++ demoCode/HtmlRead.py
@@ -1,10 +1,24 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError
 
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        # html.read()读取HTML内容
+        bsobj = BeautifulSoup(html.read())
+        #title = bsobj.body.h1
+        title = bsobj.contents
+    except AttributeError as e:
+        return None
 
-#html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-html = urlopen("http://www.sina.com")
-#html.read()读取HTML内容
-bsobj = BeautifulSoup(html.read())
-#打印网页的js脚本
-print(bsobj.script)
+    return title
+
+title = getTitle("http://data.stats.gov.cn/tablequery.htm?m=QueryData&code=AD0D&wds=%5B%7B%22wdcode%22%3A%22reg%22%2C%22valuecode%22%3A%22000000%22%7D%5D")
+if title == None:
+    print("Title could not be found")
+else:
+    print(title)
